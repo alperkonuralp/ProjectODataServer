41ca8c84c13fdabf811f8d8b463eac65411a853c
Alper Konuralp <alper@konuralp.gen.tr>
2020-09-27 21:12:42 +0300

    #Episodes 6 - We write and compile the controllers on runtime.

--- src/ProjectODataServer/ProjectODataServer/Controllers/OData/CategoryController.cs
+++ src/ProjectODataServer/ProjectODataServer/Controllers/OData/CategoryController.cs
@@ -1,27 +1,24 @@
-﻿using Microsoft.AspNet.OData;
-using Microsoft.AspNetCore.Mvc;
-using Sample.Data.DbContexts;
-using Sample.Data.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿//using Microsoft.AspNet.OData;
+//using Microsoft.AspNetCore.Mvc;
+//using Sample.Data.DbContexts;
+//using Sample.Data.Entities;
+//using System.Linq;
 
-namespace ProjectODataServer.Controllers.OData
-{
-	public class CategoryController : ControllerBase
-	{
-		private readonly SampleDataDbContext _db;
+//namespace ProjectODataServer.Controllers.OData
+//{
+//	public class CategoryController : ControllerBase
+//	{
+//		private readonly SampleDataDbContext _db;
 
-		public CategoryController(SampleDataDbContext db)
-		{
-			_db = db;
-		}
+//		public CategoryController(SampleDataDbContext db)
+//		{
+//			_db = db;
+//		}
 
-		[EnableQuery]
-		public IQueryable<Category> Get()
-		{
-			return _db.Categories;
-		}
-	}
-}
+//		[EnableQuery]
+//		public IQueryable<Category> Get()
+//		{
+//			return _db.Categories;
+//		}
+//	}
+//}
--- src/ProjectODataServer/ProjectODataServer/Controllers/OData/ProductController.cs
+++ src/ProjectODataServer/ProjectODataServer/Controllers/OData/ProductController.cs
@@ -1,24 +1,24 @@
-﻿using Microsoft.AspNet.OData;
-using Microsoft.AspNetCore.Mvc;
-using Sample.Data.DbContexts;
-using Sample.Data.Entities;
-using System.Linq;
+﻿//using Microsoft.AspNet.OData;
+//using Microsoft.AspNetCore.Mvc;
+//using Sample.Data.DbContexts;
+//using Sample.Data.Entities;
+//using System.Linq;
 
-namespace ProjectODataServer.Controllers.OData
-{
-	public class ProductController : ControllerBase
-	{
-		private readonly SampleDataDbContext _db;
+//namespace ProjectODataServer.Controllers.OData
+//{
+//	public class ProductController : ControllerBase
+//	{
+//		private readonly SampleDataDbContext _db;
 
-		public ProductController(SampleDataDbContext db)
-		{
-			_db = db;
-		}
+//		public ProductController(SampleDataDbContext db)
+//		{
+//			_db = db;
+//		}
 
-		[EnableQuery]
-		public IQueryable<Product> Get()
-		{
-			return _db.Products;
-		}
-	}
-}
+//		[EnableQuery]
+//		public IQueryable<Product> Get()
+//		{
+//			return _db.Products;
+//		}
+//	}
+//}
