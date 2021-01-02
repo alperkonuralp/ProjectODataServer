d3b56c5d4deae05650e227bd9ba76177fe15cc99
Alper Konuralp <alper@konuralp.gen.tr>
2021-01-02 15:45:42 +0300

    #Episode 29

--- src/ProjectODataServer/ProjectODataServer/Controllers/OData/CategoryController.cs
+++ src/ProjectODataServer/ProjectODataServer/Controllers/OData/CategoryController.cs
@@ -3,31 +3,37 @@ using Microsoft.AspNet.OData.Query;
 using Microsoft.AspNetCore.Mvc;
 using ProjectODataServer.Services;
 using Sample.Data.Entities;
+using System.Linq;
 
 namespace ProjectODataServer.Controllers.OData
 {
 	public class CategoryController : SampleControllerBase<Category, int>
 	{
+		#region Category
+
 		[HttpGet]
-		public IActionResult GetName(int key, ODataQueryOptions<Product> options, [FromServices] IDataService<Category, int> dataService)
+		public IActionResult GetName(int key,
+															 ODataQueryOptions<Product> options,
+															 [FromServices] IDataService<Category, int> dataService)
 		{
 			return GetProperty(key, dataService, x => x.Name);
 		}
 
+		#endregion Category
+
+		#region ServiceCategory
+
 		[EnableQuery]
-		public IActionResult GetFromServiceCategory(ODataQueryOptions<Category> options, [FromServices] IDataService<ServiceCategory, int> dataService)
+		public IActionResult GetFromServiceCategory(ODataQueryOptions<Category> options,
+																							[FromServices] IDataService<ServiceCategory, int> dataService)
 		{
 			return Ok(dataService.Get());
 		}
 
 		[EnableQuery]
-		public IActionResult GetFromShoppingCategory(ODataQueryOptions<Category> options, [FromServices] IDataService<ShoppingCategory, int> dataService)
-		{
-			return Ok(dataService.Get());
-		}
-
-		[EnableQuery]
-		public IActionResult GetServiceCategory(int key, ODataQueryOptions<Category> options, [FromServices] IDataService<ServiceCategory, int> dataService)
+		public IActionResult GetServiceCategory(int key,
+																			ODataQueryOptions<Category> options,
+																			[FromServices] IDataService<ServiceCategory, int> dataService)
 		{
 			try
 			{
@@ -39,8 +45,78 @@ namespace ProjectODataServer.Controllers.OData
 			}
 		}
 
+		[HttpGet]
 		[EnableQuery]
-		public IActionResult GetShoppingCategory(int key, ODataQueryOptions<Category> options, [FromServices] IDataService<ShoppingCategory, int> dataService)
+		public IActionResult GetProductsFromServiceCategory(int key,
+																										 ODataQueryOptions<Product> options,
+																										 [FromServices] IDataService<ServiceCategory, int> dataService)
+		{
+			try
+			{
+				var item = dataService.Get(key).SelectMany(x => x.Products);
+				if (!item.Any()) return NotFound();
+
+				return Ok(item);
+			}
+			catch (NotFoundException)
+			{
+				return NotFound();
+			}
+		}
+
+		[HttpGet]
+		[EnableQuery]
+		public IActionResult GetParentFromServiceCategory(int key,
+																										 ODataQueryOptions<Product> options,
+																										 [FromServices] IDataService<ServiceCategory, int> dataService)
+		{
+			try
+			{
+				var item = dataService.Get(key).Select(x => x.Parent);
+				if (!item.Any()) return NotFound();
+
+				return Ok(SingleResult.Create(item));
+			}
+			catch (NotFoundException)
+			{
+				return NotFound();
+			}
+		}
+
+		[HttpGet]
+		[EnableQuery]
+		public IActionResult GetChildrenFromServiceCategory(int key,
+																										 ODataQueryOptions<Product> options,
+																										 [FromServices] IDataService<ServiceCategory, int> dataService)
+		{
+			try
+			{
+				var item = dataService.Get(key).SelectMany(x => x.Children);
+				if (!item.Any()) return NotFound();
+
+				return Ok(item);
+			}
+			catch (NotFoundException)
+			{
+				return NotFound();
+			}
+		}
+
+		#endregion ServiceCategory
+
+		#region ShoppingCategory
+
+		[EnableQuery]
+		public IActionResult GetFromShoppingCategory(ODataQueryOptions<Category> options,
+																					 [FromServices] IDataService<ShoppingCategory, int> dataService)
+		{
+			return Ok(dataService.Get());
+		}
+
+		[EnableQuery]
+		public IActionResult GetShoppingCategory(int key,
+																					 ODataQueryOptions<Category> options,
+																					 [FromServices] IDataService<ShoppingCategory, int> dataService)
 		{
 			try
 			{
@@ -52,12 +128,63 @@ namespace ProjectODataServer.Controllers.OData
 			}
 		}
 
-		//[EnableQuery]
-		//public IActionResult GetProducts(int key, ODataQueryOptions<Category> options)
-		//{
-		//	if (!_db.Set<Category>().Any(x => x.Id == key)) return NotFound();
+		[HttpGet]
+		[EnableQuery]
+		public IActionResult GetProductsFromShoppingCategory(int key,
+																										 ODataQueryOptions<Product> options,
+																										 [FromServices] IDataService<ShoppingCategory, int> dataService)
+		{
+			try
+			{
+				var item = dataService.Get(key).SelectMany(x => x.Products);
+				if (!item.Any()) return NotFound();
 
-		//	return Ok(_db.Set<Product>().Where(x => x.CategoryId == key));
-		//}
+				return Ok(item);
+			}
+			catch (NotFoundException)
+			{
+				return NotFound();
+			}
+		}
+
+		[HttpGet]
+		[EnableQuery]
+		public IActionResult GetParentFromShoppingCategory(int key,
+																										 ODataQueryOptions<Product> options,
+																										 [FromServices] IDataService<ShoppingCategory, int> dataService)
+		{
+			try
+			{
+				var item = dataService.Get(key).Select(x => x.Parent);
+				if (!item.Any()) return NotFound();
+
+				return Ok(SingleResult.Create(item));
+			}
+			catch (NotFoundException)
+			{
+				return NotFound();
+			}
+		}
+
+		[HttpGet]
+		[EnableQuery]
+		public IActionResult GetChildrenFromShoppingCategory(int key,
+																										 ODataQueryOptions<Product> options,
+																										 [FromServices] IDataService<ShoppingCategory, int> dataService)
+		{
+			try
+			{
+				var item = dataService.Get(key).SelectMany(x => x.Children);
+				if (!item.Any()) return NotFound();
+
+				return Ok(item);
+			}
+			catch (NotFoundException)
+			{
+				return NotFound();
+			}
+		}
+
+		#endregion ShoppingCategory
 	}
 }
